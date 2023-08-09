159b2233cad9b29b8d215254fe149a2d6750c160
Teerawat Wuttiwat <tw@wieldmore.com>
2023-08-09 18:03:41 +0700

    add issues for DateTimeOffset

--- README.md
+++ README.md
@@ -5,7 +5,26 @@ Following functions are not implemented in date.py
 1. Today
 2. AddDays
 3. AddMonths
-4. SpecifyKind
-5. DaysInMonth
-6. ToString("O")
-7. UniversalTime
+4. Day
+5. Month
+6. Date
+7. Hour
+8. Minute
+9. Second
+10. MilliSecond
+11. SpecifyKind
+12. DaysInMonth
+13. UniversalTime
+14. LocalTime
+15. ToString("O")
+
+date_offset.py
+1. Day
+2. Month
+3. Date
+4. Hour
+5. Minute
+6. Second
+7. MilliSecond
+8. UniversalTime
+9. LocalTime
--- fableBuild/program.py
+++ fableBuild/program.py
@@ -1,5 +1,6 @@
 from typing import Any
-from fable_modules.fable_library.date import (now, today as today_1, add_days, add_months, specify_kind, days_in_month as days_in_month_1, to_string, day, month, date, hour, minute, second, millisecond, to_universal_time)
+from fable_modules.fable_library.date import (now, today as today_1, add_days, add_months, specify_kind, days_in_month as days_in_month_1, to_string, day, month, date, hour, minute, second, millisecond, to_universal_time, to_local_time)
+from fable_modules.fable_library.date_offset import (now as now_1, day as day_1, month as month_1, date as date_1, hour as hour_1, minute as minute_1, second as second_1, millisecond as millisecond_1, to_universal_time as to_universal_time_1, to_local_time as to_local_time_1)
 from fable_modules.fable_library.string_ import to_console
 
 current_date: Any = now()
@@ -40,7 +41,29 @@ to_console(("Local date for today is " + str(local_today)) + "")
 
 to_console(("Days in Months is " + str(days_in_month)) + "")
 
-to_console(("UniversalTime is " + str(to_universal_time(current_date))) + "")
+to_console(("Current UniversalTime is " + str(to_universal_time(current_date))) + "")
+
+to_console(("Current Offset LocalTime is " + str(to_local_time(current_date))) + "")
 
 to_console(("Current Date ToString is " + date_to_string) + "")
+
+current_date_offset: Any = now_1()
+
+to_console(("Current Offset Day is " + str(day_1(current_date_offset))) + "")
+
+to_console(("Current Offset Month is " + str(month_1(current_date_offset))) + "")
+
+to_console(("Current Offset Date is " + str(date_1(current_date_offset))) + "")
+
+to_console(("Current Offset Hour is " + str(hour_1(current_date_offset))) + "")
+
+to_console(("Current Offset Minute is " + str(minute_1(current_date_offset))) + "")
+
+to_console(("Current Offset Second is " + str(second_1(current_date_offset))) + "")
+
+to_console(("Current Offset Millisecond is " + str(millisecond_1(current_date_offset))) + "")
+
+to_console(("Current Offset UniversalTime is " + str(to_universal_time_1(current_date_offset))) + "")
+
+to_console(("Current Offset LocalTime is " + str(to_local_time_1(current_date_offset))) + "")
 
