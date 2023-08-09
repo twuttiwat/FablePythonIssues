d50bade05e9d791f20a9ae41f87039fa5b757433
Teerawat Wuttiwat <tw@wieldmore.com>
2023-08-09 15:59:47 +0700

    missing date functions

--- Program.fs
+++ Program.fs
@@ -1,2 +1,21 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System
+
+let today = DateTime.Today
+let currentDate = DateTime.Now
+let tomorrow = currentDate.AddDays(1)
+let nextMonth = currentDate.AddMonths(1)
+let localToday = DateTime.SpecifyKind(currentDate, DateTimeKind.Local)
+let daysInMonth = DateTime.DaysInMonth(2000, 1)
+
+printf $"Today is {today}"
+printf $"Tomorrow is {tomorrow}"
+printf $"Next month is {nextMonth}"
+printf $"Current Day is {currentDate.Day}"
+printf $"Current Month is {currentDate.Month}"
+printf $"Current Date is {currentDate.Date}"
+printf $"Current Hour is {currentDate.Hour}"
+printf $"Current Minute is {currentDate.Minute}"
+printf $"Current Second is {currentDate.Second}"
+printf $"Current Millisecond is {currentDate.Millisecond}"
+printf $"Local date for today is {localToday}"
+printf $"Days in Months is {daysInMonth}"
--- fableBuild/program.py
+++ fableBuild/program.py
@@ -1,4 +1,40 @@
-from fable_modules.fable_library.string_ import (to_console, printf)
+from typing import Any
+from fable_modules.fable_library.date import (today as today_1, now, add_days, add_months, specify_kind, days_in_month as days_in_month_1, day, month, date, hour, minute, second, millisecond)
+from fable_modules.fable_library.string_ import to_console
 
-to_console(printf("Hello from F#"))
+today: Any = today_1()
+
+current_date: Any = now()
+
+tomorrow: Any = add_days(current_date, 1)
+
+next_month: Any = add_months(current_date, 1)
+
+local_today: Any = specify_kind(current_date, 2)
+
+days_in_month: int = days_in_month_1(2000, 1)
+
+to_console(("Today is " + str(today)) + "")
+
+to_console(("Tomorrow is " + str(tomorrow)) + "")
+
+to_console(("Next month is " + str(next_month)) + "")
+
+to_console(("Current Day is " + str(day(current_date))) + "")
+
+to_console(("Current Month is " + str(month(current_date))) + "")
+
+to_console(("Current Date is " + str(date(current_date))) + "")
+
+to_console(("Current Hour is " + str(hour(current_date))) + "")
+
+to_console(("Current Minute is " + str(minute(current_date))) + "")
+
+to_console(("Current Second is " + str(second(current_date))) + "")
+
+to_console(("Current Millisecond is " + str(millisecond(current_date))) + "")
+
+to_console(("Local date for today is " + str(local_today)) + "")
+
+to_console(("Days in Months is " + str(days_in_month)) + "")
 
