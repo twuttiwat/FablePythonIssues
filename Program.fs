1623e407b95d7d5da9a872eaa84654f8721adf59
Teerawat Wuttiwat <tw@wieldmore.com>
2023-08-09 16:53:22 +0700

    add UniversalTime and ToString issue

--- Program.fs
+++ Program.fs
@@ -1,11 +1,12 @@
 ﻿open System
 
-let today = DateTime.Today
 let currentDate = DateTime.Now
+let today = DateTime.Today
 let tomorrow = currentDate.AddDays(1)
 let nextMonth = currentDate.AddMonths(1)
 let localToday = DateTime.SpecifyKind(currentDate, DateTimeKind.Local)
 let daysInMonth = DateTime.DaysInMonth(2000, 1)
+let dateToString = currentDate.ToString("O")
 
 printf $"Today is {today}"
 printf $"Tomorrow is {tomorrow}"
@@ -19,3 +20,5 @@ printf $"Current Second is {currentDate.Second}"
 printf $"Current Millisecond is {currentDate.Millisecond}"
 printf $"Local date for today is {localToday}"
 printf $"Days in Months is {daysInMonth}"
+printf $"UniversalTime is {currentDate.ToUniversalTime()}"
+printf $"Current Date ToString is {dateToString}"
--- fableBuild/program.py
+++ fableBuild/program.py
@@ -1,10 +1,10 @@
 from typing import Any
-from fable_modules.fable_library.date import (today as today_1, now, add_days, add_months, specify_kind, days_in_month as days_in_month_1, day, month, date, hour, minute, second, millisecond)
+from fable_modules.fable_library.date import (now, today as today_1, add_days, add_months, specify_kind, days_in_month as days_in_month_1, to_string, day, month, date, hour, minute, second, millisecond, to_universal_time)
 from fable_modules.fable_library.string_ import to_console
 
-today: Any = today_1()
-
 current_date: Any = now()
+
+today: Any = today_1()
 
 tomorrow: Any = add_days(current_date, 1)
 
@@ -13,6 +13,8 @@ next_month: Any = add_months(current_date, 1)
 local_today: Any = specify_kind(current_date, 2)
 
 days_in_month: int = days_in_month_1(2000, 1)
+
+date_to_string: str = to_string(current_date, "O")
 
 to_console(("Today is " + str(today)) + "")
 
@@ -37,4 +39,8 @@ to_console(("Current Millisecond is " + str(millisecond(current_date))) + "")
 to_console(("Local date for today is " + str(local_today)) + "")
 
 to_console(("Days in Months is " + str(days_in_month)) + "")
+
+to_console(("UniversalTime is " + str(to_universal_time(current_date))) + "")
+
+to_console(("Current Date ToString is " + date_to_string) + "")
 
